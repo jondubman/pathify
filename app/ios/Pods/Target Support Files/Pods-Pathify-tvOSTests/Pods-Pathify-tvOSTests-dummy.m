11b00a9d3f0efc5c3cd176f8f1741f2b652510f1
jondubman <jon@dubman.com>
2018-11-29 16:21:34 -0800

    Add react-native-mapbox-gl package, CocoaPods Podfile for Mapbox integration. Generate ios/Pathify.xcworkspace.

---
+++ app/ios/Pods/Target Support Files/Pods-Pathify-tvOSTests/Pods-Pathify-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pathify_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Pathify_tvOSTests
+@end
---
+++ app/ios/Pods/Target Support Files/Pods-PathifyTests/Pods-PathifyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PathifyTests : NSObject
+@end
+@implementation PodsDummy_Pods_PathifyTests
+@end
