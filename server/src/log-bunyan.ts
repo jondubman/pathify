9703e514877d577b1fecf4140c9281bd0f19482f
jondubman <jon@dubman.com>
2018-12-09 17:38:18 -0800

    server: Scaffolding for Node/TypeScript server with Express, logging with bunyan

---
+++ server/src/log-bunyan.ts
@@ -0,0 +1,80 @@
+import * as bunyan from 'bunyan';
+import * as fs from 'fs';
+
+const latestLogFile = './latest.log';
+const serverLogfile = './server.log';
+const completeLogFile = './complete.log';
+
+// tail -f server.log | bunyan
+// `type: 'file'` is implied
+// can also leave it open in Sublime Text, clear, save, etc.
+
+// always clear latestLogFile on startup
+try {
+  fs.unlinkSync(latestLogFile);
+} catch (err) { } // we don't care if it fails
+
+const bunyanLogger = bunyan.createLogger({
+  name: 'server',
+  hostname: 'host', // TODO
+  level: 0,
+  streams: [{ path: latestLogFile }, { path: serverLogfile }, { path: completeLogFile }],
+}) as any;
+
+// map null to 'null' else Bunyan will not show anything at all for null
+const nullToString = e => (e === null ? 'null' : e);
+
+const log = {
+  bunyanLogger,
+  latestLogFile,
+  serverLogfile,
+
+  trace: (...args) => {
+    const fixedArgs = args.map(nullToString);
+    bunyanLogger.trace(...fixedArgs);
+  },
+  debug: (...args) => {
+    const fixedArgs = args.map(nullToString);
+    bunyanLogger.debug(...fixedArgs);
+  },
+  info: (...args) => {
+    const fixedArgs = args.map(nullToString);
+    bunyanLogger.info(...fixedArgs);
+  },
+  warn: (...args) => {
+    const fixedArgs = args.map(nullToString);
+    bunyanLogger.warn(...fixedArgs);
+  },
+  error: (...args) => {
+    const fixedArgs = args.map(nullToString);
+    bunyanLogger.error(...fixedArgs);
+  },
+  fatalError: (...args) => {
+    const fixedArgs = args.map(nullToString);
+    bunyanLogger.fatal(...fixedArgs);
+  },
+  // TODO implement in log-winston as well
+  exception: (e, message) => log.error({ err: e, message: 'exception: ' + message }),
+}
+
+// function testLogging() {
+//   log.info(undefined);
+//   log.info(null); // TODO this shows as nothing at all; would prefer to see 'null'
+//   log.info(false);
+//   log.info(true);
+//   log.info('this is a test');
+//   log.info('this', 'is', 'another', 'test');
+//   log.info('hi %s', 'Jonathan'); // uses util.format
+//   log.info({a: 123, b: 456});
+//   log.info({obj: {x:'foo', y:'bar'}});
+
+//   log.trace('some trace message');
+//   log.debug('some debug message');
+//   log.warn('some warning message');
+//   log.error('some error message');
+//   log.fatalError('some fatal error message');
+// }
+
+// testLogging();
+
+module.exports = log;
---
+++ server/src/server.ts
@@ -0,0 +1,86 @@
+// Pathify server main
+
+import * as bodyParser from 'body-parser'; // Unbundled from Express as of Express 4
+import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
+import * as helmet from 'helmet';
+// import * as https from 'https';
+// import * as JSON5 from 'json5'; // extension of JSON with more permissive syntax
+
+const bunyanMiddleware = require('bunyan-middleware');
+
+// const constants = require('./constants');
+// const database = require('./database');
+// const datetime = require('./datetime');
+const log = require('./log-bunyan');
+// const utils = require('./utils');
+
+const app = express();
+
+app.use(helmet()); // https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
+// NOTE: This must be done BEFORE wiring app.post handler in order to receive anything in req.body!
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use(bunyanMiddleware({
+  headerName: 'X-Request-Id',
+  propertyName: 'reqId',
+  logName: 'req_id',
+  logger: log.bunyanLogger,
+  level: 'trace',
+  }
+))
+
+// used for fatal error / server restart
+function flushLogsAndExit(msecDelay = 1000) {
+  // Note this would exit immediately, omitting logs due to https://github.com/trentm/node-bunyan/issues/95
+  // process.exit(1);
+
+  // TODO this seems hackish; would prefer to ensure outgoing logs are flushed first, but that feature is TBD
+  // in node-bunyan. In practice, 100 msec seems sufficient; choosing larger default to be safe.
+
+  log.info(`shutting down in ${msecDelay}ms...`);
+  setTimeout(() => {
+    log.info('shutting down NOW!');
+    process.exit(1);
+  }, msecDelay);
+}
+
+// ---SECTION: Startup
+
+// This is called synchronously below. It's only async so it can await.
+const startServer = async () => {
+  log.info('--------------------------');
+}
+
+// ---SECTION: Top-level code
+
+// https://stackoverflow.com/questions/40867345/catch-all-uncaughtexception-for-node-js-app
+process
+  .on('unhandledRejection', (reason, p) => {
+    // log.fatalError(reason, 'Unhandled Rejection at Promise', p);
+    log.fatalError(reason, 'Unhandled Rejection at Promise');
+    flushLogsAndExit();
+  })
+  .on('uncaughtException', err => {
+    log.fatalError(err, 'Uncaught Exception thrown');
+    flushLogsAndExit();
+  })
+
+// Important: Actually start the server!
+
+startServer();
+
+// TODO - experimental timer-based failure injection
+//
+// setTimeout(() => {
+//   throw new Error('timebomb');
+// }, 5000)
