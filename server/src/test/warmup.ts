72844d2aae52cfe9a7bd8edd98f45b88efe4ef5d
jondubman <jon@dubman.com>
2019-04-25 17:27:44 -0700

    server: Add Prisma / GraphQL / MongoDB / Docker with datamodel for time series of events, with metadata, anticipating two-way synchronization, auto-generating client code in TypeScript. Update packages, build scripts, Mocha test script.

--- server/src/test/warmup.ts
+++ server/src/test/warmup.ts
@@ -11,6 +11,17 @@ import { constants } from 'lib/constants';
 
 describe('category-warmup', function() {
   it(`should confirm server port number ${constants.defaultPort}`, async function() {
+    console.log('hello');
     expect(constants.defaultPort).to.exist;
+  })
+})
+
+import { prisma } from 'prisma-client/index';
+
+describe('category-prisma', function () {
+  it(`should run a query`, async function () {
+    const testResult = await prisma.events();
+    console.log(testResult);
+    expect(testResult).to.exist;
   })
 })
