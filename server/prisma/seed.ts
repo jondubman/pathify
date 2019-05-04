bf54a8658850035c4445d3f591097d1e646735a4
jondubman <jon@dubman.com>
2019-05-03 17:38:37 -0700

    server: package upgrades

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -3,9 +3,34 @@ import { prisma } from './generated/prisma-client'
 
 async function main() {
   await prisma.createEvent({
-    t: 1000
+    t: 1000,
   })
   await prisma.createEvent({
+    t: 2000,
+  })
+  await prisma.updateManyEvents({
+    data: {
+      t: 2001,
+      data: {x: 123, y: 456},
+    },
+    where: {
+      t: 2000
+    },
+  })
+  await prisma.createEvent({
+    t: 2000,
+    data: {x: 789},
+  })
+  await prisma.updateManyEvents({
+    data: {
+      t: 2000,
+      data: { x: 101112 },
+    },
+    where: {
+      t: 2000
+    },
+  })
+  await prisma.deleteManyEvents({
     t: 2000
   })
 }
