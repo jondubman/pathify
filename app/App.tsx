f2a16e0bdb28e5e64bf822ad0e6e7968e1c2e707
jondubman <jon@dubman.com>
2019-05-14 17:51:11 -0700

    app: Fix geolocation options on startup

--- app/App.tsx
+++ app/App.tsx
@@ -23,7 +23,7 @@ export default class App extends Component {
     log.debug('windowSize', utils.windowSize());
     store.create(); // proactively create Redux store instance
 
-    Geo.initializeGeolocation(store); // TODO use only when needed
+    Geo.initializeGeolocation(store);
     Geo.resetOdometer();
     store.dispatch(newAction(appAction.START_FOLLOWING_USER));
 
