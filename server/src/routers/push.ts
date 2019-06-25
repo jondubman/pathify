fd4ec51c0bbf00cd5d46c8c60fcb18a4af6cbf3c
jondubman <jon@dubman.com>
2019-06-25 10:13:04 -0700

    server: handle string responses from app

--- server/src/routers/push.ts
+++ server/src/routers/push.ts
@@ -62,17 +62,23 @@ router.post('/', function (req, res) {
           reject('timeout');
         }, timeout);
       }).then(appQueryResponse => {
-        const responseReadable = util.inspect(appQueryResponse.response, { depth: 4 });
+        const { response } = appQueryResponse;
+        const responseReadable = util.inspect(response, { depth: 4 });
         log.info(`response from clientId ${clientId}: ${responseReadable}`);
         // forward response to whoever requested that we post this JSON to the app
-        res.send(JSON.stringify(appQueryResponse.response));
-        return appQueryResponse.response; // TODO do we need to return anything?
+        if (typeof response === 'string') {
+          res.send(response);
+        } else {
+          res.send(JSON.stringify(response));
+        }
+        // return response; // TODO do we need to return anything?
       }).catch(err => {
         log.info(`appQuery timeout clentId ${clientId}`);
         res.send('timeout');
       })
     }
   }
+  // Push message to client (whether appQuery, or any other)
   const inspect = JSON.stringify(message);
   log.debug(`push object to clientAlias ${clientAlias}, clientId ${clientId}, message ${inspect}`);
   pushToClient(message, clientId, clientAlias);
