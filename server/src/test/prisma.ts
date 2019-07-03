1511da39be4d67321e2b8509d5d14148bbb9c70b
jondubman <jon@dubman.com>
2019-07-02 19:05:09 -0700

    mocha test for Prisma now skippable via process.env.PATHIFY_MODE

--- server/src/test/prisma.ts
+++ server/src/test/prisma.ts
@@ -11,25 +11,27 @@ import { continuousTracks } from 'shared/tracks';
 // Mocha tests begin here
 // ----------------------
 
-// TODO describe.skip
+// TODO for now, skip Prisma tests when running locally
 
-describe.skip('prisma', function () {
+if (process.env.PATHIFY_MODE === 'production') {
+  describe('Prisma', function () {
 
-  it('should run a query', async function () {
-    const testResult = await prisma.events();
-    expect(testResult).to.exist;
+    it('should run a query', async function () {
+      const testResult = await prisma.events();
+      expect(testResult).to.exist;
+    })
+
+    it('should count events', async function () {
+      const events: GenericEvents = await prisma.events() as GenericEvents;
+      const count = timeseries.countEvents(events);
+      console.log('count of events', count);
+      expect(count).to.exist;
+    })
+
+    it('should calculate continuousTracks', async function () {
+      const events: GenericEvents = await prisma.events() as GenericEvents;
+      const tracks = continuousTracks(events, 1);
+      expect(tracks).to.exist;
+    })
   })
-
-  it('should count events', async function () {
-    const events: GenericEvents = await prisma.events() as GenericEvents;
-    const count = timeseries.countEvents(events);
-    // console.log('count', count);
-    expect(count).to.exist;
-  })
-
-  it('should calculate continuousTracks', async function () {
-    const events: GenericEvents = await prisma.events() as GenericEvents;
-    const tracks = continuousTracks(events, 1);
-    expect(tracks).to.exist;
-  })
-})
+}
