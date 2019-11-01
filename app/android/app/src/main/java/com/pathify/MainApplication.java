3cbaa33809e03706a357f80a6e792a9f3c5db394
jondubman <jon@dubman.com>
2019-11-01 12:40:25 -0700

    Upgrade manually to RN 0.61.2

--- app/android/app/src/main/java/com/pathify/MainApplication.java
+++ app/android/app/src/main/java/com/pathify/MainApplication.java
@@ -3,6 +3,7 @@ package com.pathify;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.avishayil.rnrestart.ReactNativeRestartPackage;
 import io.realm.react.RealmReactPackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeRestartPackage(),
             new RealmReactPackage(),
             new ReactSliderPackage(),
             new RCTMGLPackage(),
