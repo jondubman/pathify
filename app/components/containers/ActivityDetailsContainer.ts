cdff029c7c926440ebce51186c1fef4770e5b49e
jondubman <jon@dubman.com>
2020-02-05 15:38:08 -0800

    Refine ActivityDetailsContainer mapStateToProps

--- app/components/containers/ActivityDetailsContainer.ts
+++ app/components/containers/ActivityDetailsContainer.ts
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import {
   dynamicTopBelowActivityList,
   getCachedPathInfo,
-  getScrollTimeRounded,
 } from 'lib/selectors';
 import { AppState } from 'lib/state';
 import ActivityDetails from 'presenters/ActivityDetails';
@@ -40,78 +39,82 @@ interface ActivityDetailsDispatchProps {
 export type ActivityDetailsProps = ActivityDetailsStateProps & ActivityDetailsDispatchProps;
 
 const mapStateToProps = (state: AppState): ActivityDetailsStateProps => {
-  const top = dynamicTopBelowActivityList(state);
-  const info = getCachedPathInfo(state);
-  const { currentActivityId } = state.options;
-  const scrollTime = getScrollTimeRounded(state);
-  const { timelineNow } = state.flags;
-
-  let partialDistance = 0;
-  let totalDistance = 0;
-  let totalTime = 0;
-
   let averagePaceText = '';
   let averageSpeedText = '';
   let distanceText = '';
   let elevationText = '';
   let paceText = '0';
+  let partialDistance = 0;
   let speedText = '';
   let timeText = '';
-  if (info) {
-    const activity = info.activity as ActivityDataExtended;
-    const isCurrent = activity && (activity.id === currentActivityId);
-    if (activity) {
-      if (activity.odo && activity.odoStart) {
-        totalDistance = Math.max(activity.odo - activity.odoStart, 0);
+  let totalDistance = 0;
+  let top = 0;
+  let totalTime = 0;
+  const { timelineNow } = state.flags;
+  const defaults = {
+    averagePaceText,
+    averageSpeedText,
+    distanceText,
+    elevationText,
+    isCurrent: false,
+    paceText,
+    speedText,
+    timelineNow,
+    timeText,
+    top,
+    visible: true,
+  }
+  try {
+    top = dynamicTopBelowActivityList(state);
+    const info = getCachedPathInfo(state);
+    const { currentActivityId, scrollTime } = state.options;
+
+    if (info) {
+      const activity = info.activity as ActivityDataExtended;
+      const isCurrent = activity && (activity.id === currentActivityId);
+      if (activity) {
+        if (activity.odo && activity.odoStart) {
+          totalDistance = Math.max(activity.odo - activity.odoStart, 0);
+        }
+        if (activity.tStart) {
+          timeText = msecToTimeString(scrollTime - activity.tStart);
+        }
+        if (info.odo && activity.odoStart) {
+          partialDistance = Math.max(info.odo - activity.odoStart, 0); // meters
+          distanceText = metersToMilesText(partialDistance, '');
+        }
+        if (info.pace) {
+          paceText = minutesToString(metersPerSecondToMinutesPerMile(info.pace));
+        }
+        // TODO support legitimate negative elevation. -1 is getting reported for elevation in the Simulator.
+        elevationText = (!info.ele || info.ele < 0) ? '' : metersToFeet(info.ele).toFixed(0);
+        totalTime = activity.tLast - activity.tStart;
+        if (totalDistance && totalTime) {
+          const mps = totalDistance / (totalTime / 1000);
+          const averagePace = metersPerSecondToMinutesPerMile(mps);
+          averagePaceText = minutesToString(averagePace);
+          averageSpeedText = metersPerSecondToMilesPerHour(mps).toFixed(1);
+        }
       }
-      if (activity.tStart) {
-        const t = scrollTime;
-        timeText = msecToTimeString(t - activity.tStart);
+      return {
+        averagePaceText,
+        averageSpeedText,
+        distanceText,
+        elevationText,
+        isCurrent,
+        paceText,
+        speedText,
+        timelineNow,
+        timeText,
+        top,
+        visible: true,
       }
-      if (info.odo && activity.odoStart) {
-        partialDistance = Math.max(info.odo - activity.odoStart, 0); // meters
-        distanceText = metersToMilesText(partialDistance, '');
-      }
-      if (info.pace) {
-        paceText = minutesToString(metersPerSecondToMinutesPerMile(info.pace));
-      }
-      // TODO support legitimate negative elevation. -1 is getting reported for elevation in the Simulator.
-      elevationText = (!info.ele || info.ele < 0) ? '' : metersToFeet(info.ele).toFixed(0);
-      totalTime = activity.tLast - activity.tStart;
-      if (totalDistance && totalTime) {
-        const mps = totalDistance / (totalTime / 1000);
-        const averagePace = metersPerSecondToMinutesPerMile(mps);
-        averagePaceText = minutesToString(averagePace);
-        averageSpeedText = metersPerSecondToMilesPerHour(mps).toFixed(1);
-      }
+    } else {
+      return defaults;
     }
-    return {
-      averagePaceText,
-      averageSpeedText,
-      distanceText,
-      elevationText,
-      isCurrent,
-      paceText,
-      speedText,
-      timelineNow,
-      timeText,
-      top,
-      visible: true,
-    }
-  } else {
-    return {
-      averagePaceText,
-      averageSpeedText,
-      distanceText,
-      elevationText,
-      isCurrent: false,
-      paceText,
-      speedText,
-      timelineNow,
-      timeText,
-      top,
-      visible: false, // this makes the rest basically irrelevant
-    }
+  } catch(err) {
+    log.warn('Exception in ActivityDetailsContainer mapStateToProps', err);
+    return defaults;
   }
 }
 
