254cfecda373031d89283d7173787155a23c973a
jondubman <jon@dubman.com>
2020-08-18 13:42:02 -0700

    Clean up rn-cli.config. Package updates.

--- app/rn-cli.config.js
+++ app/rn-cli.config.js
@@ -1,18 +1,18 @@
-// This is the key to getting Typescript to work with react-native.
+// This is (TODO - was!) the key to getting Typescript to work with react-native.
 module.exports = {
-  assets: ['react-native-vector-icons'],
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false
-      }
-    })
-  },
-  getTransformModulePath() {
-    return require.resolve('react-native-typescript-transformer');
-  },
-  getSourceExts() {
-    return ['ts', 'tsx'];
-  },
+  // assets: ['react-native-vector-icons'],
+  // transformer: {
+  //   getTransformOptions: async () => ({
+  //     transform: {
+  //       experimentalImportSupport: false,
+  //       inlineRequires: false
+  //     }
+  //   })
+  // },
+  // getTransformModulePath() {
+  //   return require.resolve('react-native-typescript-transformer');
+  // },
+  // getSourceExts() {
+  //   return ['ts', 'tsx'];
+  // },
 };
