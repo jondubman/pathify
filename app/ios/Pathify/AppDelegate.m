7649eff87714f4c5a1ce92dccd1a035dd1d5760c
jondubman <jon@dubman.com>
2020-08-25 14:57:05 -0700

    Avoid white screen flash upon loading LaunchScreen.storyboard. Restore AppDelegate code for background fetch, and custom code involving environment variables passed on launch.

--- app/ios/Pathify/AppDelegate.m
+++ app/ios/Pathify/AppDelegate.m
@@ -4,6 +4,12 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <TSBackgroundFetch/TSBackgroundFetch.h>
+
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -31,18 +37,52 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"Pathify"
-                                            initialProperties:nil];
+  NSURL *jsCodeLocation;
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+// https://facebook.github.io/react-native/docs/running-on-device
+#ifdef DEBUG
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 
+  RCTBridge *bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation
+    moduleProvider:nil
+    launchOptions:launchOptions];
+
+#if RCT_DEV
+[bridge moduleForClass:[RCTDevLoadingView class]];
+#endif
+
+  //  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+  //                                                   moduleName:@"Pathify"
+  //                                            initialProperties:nil];
+
+  // Custom code for Pathify: retrieve the environment variables for the running app and pass them to the RCTRootView.
+  NSDictionary *environment = [[NSProcessInfo processInfo] environment];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"Pathify"
+                                               initialProperties:environment
+                                                   launchOptions:launchOptions];
+
+  // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:0.0f blue:0.0f alpha:1];
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  // Pro Tip
+  // As your App Bundle grows in size, you may start to see a white screen flash between your splash screen and the
+  // display of your root application view. If this is the case, you can add the following code to AppDelegate.m
+  // in order to keep your splash screen displayed during the transition.
+  UIView* launchScreenView = [[[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex:0];
+  launchScreenView.frame = self.window.bounds;
+  rootView.loadingView = launchScreenView;
+
+  // [REQUIRED] Register BackgroundFetch
+  [[TSBackgroundFetch sharedInstance] didFinishLaunching];
+
   return YES;
 }
 
