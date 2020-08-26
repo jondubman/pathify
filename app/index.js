3a18047986016f5def6b666a53ebd4b23d459c5a
jondubman <jon@dubman.com>
2020-08-26 15:10:39 -0700

    Add react-native-get-random-values polyfill for crypto.getRandomValues.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import { AppRegistry } from 'react-native';
+import 'react-native-get-random-values'; // polyfill for crypto.getRandomValues
 import App from './App';
 import { name as appName } from './app.json';
 
