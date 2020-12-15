8b8da5d6802eb51cfa41ac489f1df2cde5b3969f
jondubman <jon@dubman.com>
2020-12-15 12:49:08 -0800

    Add sample0..4 to SampleData.xcassets. Add PathifyNative.swift, called from AppDelegate.m to load sample data into initialProperties NSMutableDictionary for the RN app. JSON is parsed in App.tsx when the main component mounts. sample data is in the main bundle for the Pthify target. Should exclude it from non-test scenarios.

---
+++ app/ios/PathifyNative.swift
@@ -0,0 +1,35 @@
+import Foundation
+import UIKit
+@objc public class PathifyNative: NSObject {
+  public override init()
+  {
+    super.init()
+  }
+
+  deinit
+  {
+  }
+
+  @objc public static func foo() -> String {
+    return "bar"
+  }
+
+  @objc public static func loadSampleData(props: NSMutableDictionary) {
+    // print(props)
+
+    let testBundle = Bundle.main
+    var index = 0
+    // var samples: [String : String] = [:]
+    repeat {
+      let name = "sample" + String(index)
+      guard let asset = NSDataAsset(name: name, bundle: testBundle) else {
+        break; // this is ok, we are simply done with samples
+      }
+      // Note this json is too long to fit in the launchEnvironment!
+      print("Stringifying sample data: " + name)
+      let json = String(decoding: asset.data, as: UTF8.self)
+      props[name] = json
+      index += 1
+    } while true    
+  }
+}
